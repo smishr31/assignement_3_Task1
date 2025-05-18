4e0b7ffa1d222308405236b507af845492c01dab
smishr31 <sajalmishra910@gmail.com>
2025-05-19 00:05:23 +0530

    Add files via upload

---
+++ assignement3_Task2.py
@@ -0,0 +1,9 @@
+import math
+a = int(input("Enter Number: "))
+square = math.sqrt(a)
+l = math.log(a)
+s = math.sin(a)
+print(f"square root : {square}")
+print(f"logarithm : {l}")
+print(f"sine : {s}")
+
